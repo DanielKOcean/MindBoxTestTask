7bc0f68828af5516f5f9d90cad07e401ea7953bc
Ivan Khorin <khorin.ivan.stefanovich@outlook.com>
2022-07-14 14:01:04 +0300

    Add utils for shape area calculation

--- src/ShapesLib/ShapesLib.cs
+++ src/ShapesLib/ShapesLib.cs
@@ -1,5 +1,10 @@
-﻿namespace ShapesLib;
-public class ShapesLib
-{
+﻿using ShapesLib.Shapes;
 
+namespace ShapesLib;
+
+public class ShapeUtils
+{
+    public static double GetArea(Shape shape) => shape.Area();
+
+    public static double GetArea(List<Shape> shapes) => shapes.Aggregate(0.0, (acc, shape) => acc + shape.Area());
 }
--- tests/ShapesLib.Tests/ShapesLibTest.cs
+++ tests/ShapesLib.Tests/ShapesLibTest.cs
@@ -19,7 +19,7 @@ public class ShapesLibTests
         var triangle = new Triangle(a, b, c);
 
         var actual = triangle.Area();
-        
+
         Assert.AreEqual(expected, actual, 0.0001);
     }
 
@@ -32,7 +32,7 @@ public class ShapesLibTests
         var circle = new Circle(r);
 
         var actual = circle.Area();
-        
+
         Assert.AreEqual(expected, actual, 0.0001);
     }
 
@@ -89,5 +89,31 @@ public class ShapesLibTests
         var ex = Assert.Throws<Exception>(() => triangle.Area());
 
         Assert.That(ex?.Message, Is.EqualTo("Triangle sides dont match correct values."));
+    }
+
+    [Test]
+    [TestCase(1, 3.1416)]
+    [TestCase(5, 78.5398)]
+    [TestCase(1600.0, 8042477.1931)]
+    public void Utils_GetArea_ReturnsCorrectValueForCircle(double r, double expected)
+    {
+        var circle = new Circle(r);
+
+        var actual = ShapeUtils.GetArea(circle);
+
+        Assert.AreEqual(expected, actual, 0.0001);
+    }
+
+    [Test]
+    [TestCase(10, 10, 14.14, 50.0000)]
+    [TestCase(7, 10, 5, 16.2481)]
+    [TestCase(50, 70, 30, 649.5191)]
+    public void Utils_GetArea_ReturnsCorrectValueForTriangle(double a, double b, double c, double expected)
+    {
+        var triangle = new Triangle(a, b, c);
+
+        var actual = ShapeUtils.GetArea(triangle);
+
+        Assert.AreEqual(expected, actual, 0.0001);
     }
 }
